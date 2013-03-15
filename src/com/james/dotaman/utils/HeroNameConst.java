5789cd0259c0038b25c9b981de6e9ead89fc1e51
james026yeah <james026yeah@gmail.com>
2013-03-15 21:49:45 +0800

    add hero name

--- src/com/james/dotaman/utils/HeroNameConst.java
+++ src/com/james/dotaman/utils/HeroNameConst.java
@@ -3,18 +3,19 @@ package com.james.dotaman.utils;
 public interface HeroNameConst {
 
 	//follow is 20:
-	public static final String KUNKKA = "kunkka";
-	public static final String BM = "bm";
-	public static final String CENT = "cent";
-	public static final String ES = "es";
-	public static final String OK = "ok";
-	public static final String PANDA = "panda";
+	public static final String KUNKKA = "kunkka";//船长
+	public static final String BM = "bm";//兽王
+	public static final String CENT = "cent";//半人马酋长
+	public static final String ES = "es";//撼地神牛
+	public static final String OK = "ok";//全能骑士
+	public static final String MA = "ma";//熊猫酒仙
 	public static final String SVEN = "sven";
 	public static final String TINY = "tiny";
 	public static final String TC = "tc";
 	public static final String TP = "tp";
 	public static final String WISP = "wisp";
 	public static final String ALCH = "alch";
+	
 	public static final String CG = "cg";
 	public static final String DK = "dk";
 	public static final String SW = "sw";
@@ -24,12 +25,51 @@ public interface HeroNameConst {
 	public static final String LC = "lc";
 	public static final String GS = "gs";
 	
+	
 	//
-	public static final String AM = "am";
+	public static final String AM = "am";//敌法师
+	public static final String SNIPER = "sniper";
+	public static final String JUGG = "jugg";//剑圣
+	public static final String LD = "ld";//德鲁伊
+	public static final String LUNA = "luna";
+	public static final String MORPH = "morph";
+	public static final String NAGA = "naga";
+	public static final String LANCER = "lancer";
+	public static final String POM = "pom";
+	public static final String SA = "sa";
+	public static final String TROLL = "troll";
+	public static final String AV = "av";
+	
+	public static final String DR = "dr";
+	public static final String TA = "ta";
+	public static final String URSA = "ursa"; 
+	public static final String VS = "vs";
+	public static final String BH = "bh";
+	public static final String EM = "em";
+	
 	
 	
 	//
 	public static final String CM = "cm";
+	public static final String AS = "as";
+	public static final String PUCK = "puck";
+	public static final String CHEN ="chen";
+	public static final String KOTL = "kotl";
+	public static final String ZEUS = "zeus";
+	public static final String FURION = "furion";
+	public static final String SIL = "sil";
+	public static final String LINA = "lina";
+	public static final String STORM = "storm";
+	public static final String WR = "wr";
+	public static final String THRALL = "thrall";
+	
+	public static final String ORG = "org";
+	public static final String GOBLIN = "goblin";
+	public static final String THD = "thd";
+	public static final String TK = "tk";
+	public static final String SS = "ss";
+	public static final String GM = "gm";
+	public static final String SM = "sm";
 	
 	
 	//
